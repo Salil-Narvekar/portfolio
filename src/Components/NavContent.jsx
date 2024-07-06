7f5bd42d55823815280344aa8c7f41eea27d820e
Salil Narvekar <salil.enpointe@gmail.com>
2024-07-06 23:56:04 +0530

    commit

--- src/Components/NavContent.jsx
+++ src/Components/NavContent.jsx
@@ -6,15 +6,15 @@ const NavContent = ({ navFor, navIcon }) => {
 
     return (
         <div
-            className='grid justify-items-center items-center rounded-3xl cursor-pointer h-11 w-11 hover:bg-gradient-to-r from-transparent to-violet-500 hover:w-32 hover:ml-3 hover:scale-110 hover:justify-items-start hover:pl-4 hover:h-10 duration-500'
+            className='grid justify-items-center items-center rounded-3xl cursor-pointer h-8 w-8 lg:h-11 lg:w-11 hover:bg-gradient-to-l from-transparent to-violet-500 hover:w-32 hover:ml-3 hover:scale-110 hover:justify-items-start hover:pl-4 hover:h-8 hover:lg:h-10 duration-500'
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
             {
                 isHovered ?
-                    <span className='text-md text-slate-600 font-semibold'> {navFor} </span>
+                    <span className='text-sm lg:text-md text-slate-200 font-semibold'> {navFor} </span>
                     :
-                    <span className='text-lg text-black'> {navIcon} </span>
+                    <span className='text-md lg:text-lg text-black'> {navIcon} </span>
             }
         </div>
     )
