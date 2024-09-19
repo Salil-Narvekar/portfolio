76f4a1aa72946161d9fe3c2fadc5518b82f10dae
Salil Narvekar <salil.enpointe@gmail.com>
2024-09-19 16:51:52 +0530

    commit

--- src/Components/AboutDescription.jsx
+++ src/Components/AboutDescription.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const AboutDescription = () => {
     return (
         <div className='text-left md:text-center lg:text-center text-balance'>
-            <div className='text-sm md:text-md lg:text-lg xl:text-xl text-slate-600 font-bold font-serif'>
-                '' Dedicated to grow & evolve with curiosity & creativity in the world of web development. ''
+            <div className='text-sm md:text-md lg:text-lg xl:text-xl text-slate-600 font-extrabold font-serif'>
+                "Dedicated to grow & evolve with curiosity & creativity in the world of web development."
             </div>
             <div className='text-xs md:text-sm lg:text-md xl:text-lg text-slate-600 font-medium'> 
-                I am a <b className='text-violet-950'>Full stack web developer</b> specializing in <b className='text-violet-950'>React.js & Python, </b> along with an experience in all the technical skills mentioned below. 
+                I am a <b className='text-violet-950'>full stack web developer</b> specializing in <b className='text-violet-950'>React.js & Python, </b> along with an experience in all the technical skills mentioned below. 
             </div>
         </div>
     )
