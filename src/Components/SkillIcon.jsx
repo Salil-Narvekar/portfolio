8cc0bcb9bb58945d0c67eb196e4fe4c14fdede03
Salil Narvekar <salil.enpointe@gmail.com>
2024-11-07 23:36:15 +0530

    commit

--- src/Components/SkillIcon.jsx
+++ src/Components/SkillIcon.jsx
@@ -6,6 +6,7 @@ export const SkillIcon = ({ icon, name, setSkillName }) => {
             className='relative group'
             onMouseEnter={() => setSkillName(name)}
             onMouseLeave={() => setSkillName('')}
+            data-aos-offset="100" data-aos-mirror="true" data-aos='flip-left'
         >
             <div className='text-xl md:text-3xl lg:text-3xl xl:text-3xl h-6 w-6 md:h-7 md:w-7 lg:h-10 lg:w-10 content-center rounded-3xl drop-shadow-2xl hover:scale-105 hover:-translate-y-4 duration-500 cursor-pointer'>
                 {icon}
