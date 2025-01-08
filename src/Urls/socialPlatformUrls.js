53abc5946096dc3747a419c1997a8cd5aba2f9c9
Salil Narvekar <salil.enpointe@gmail.com>
2025-01-08 16:59:12 +0530

    commit

--- src/Urls/socialPlatformUrls.js
+++ src/Urls/socialPlatformUrls.js
@@ -3,7 +3,7 @@ const socialPlatformUrls = {
     gmailUrl: 'https://mail.google.com/mail/?view=cm&fs=1&to=narvekarsalil1106@gmail.com',
     linkedInUrl: 'https://www.linkedin.com/in/salil-narvekar/',
     gitHubUrl: 'https://github.com/Salil-Narvekar',
-    instagramUrl: 'https://www.instagram.com/salil.11._/'
+    instagramUrl: 'https://www.instagram.com/feel_the_flow._/'
 }
 
 export default socialPlatformUrls
