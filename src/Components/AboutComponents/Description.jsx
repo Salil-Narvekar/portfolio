818058238cc977704403f30b8780625c5d6c94f2
Salil Narvekar <salil.enpointe@gmail.com>
2025-11-08 18:02:51 +0530

    commit

--- src/Components/AboutComponents/Description.jsx
+++ src/Components/AboutComponents/Description.jsx
@@ -4,11 +4,11 @@ const Description = () => {
     return (
         <div className='border-gray-900 border-t rounded-xl font-semibold text-slate-900 text-balance text-xs md:text-md lg:text-md xl:text-lg py-4 pl-4 pr-2 w-full' data-aos-offset="160" data-aos-mirror="true" data-aos='fade-left'>
             Hello, I'm <b className='text-violet-950'>Salil Narvekar, </b> <br />
-            a passionate junior web developer with a love for crafting clean, efficient, and user-friendly web experiences. <br />
-            I specialize in building responsive websites and applications using modern technologies like React, Python, HTML, CSS, JavaScript, and Vue. <br />
-            My goal is to turn innovative ideas into functional digital solutions that enhance user experiences and solve real-world problems. <br />
-            Take a look at my work, and feel free to reach out if you’d like to collaborate or just chat about web development. <br />
-            I'm always excited about new challenges and opportunities to grow in this ever-evolving field."
+            a passionate data analyst and full-stack developer with a keen interest in data science, AI, and machine learning.<br />
+            I love turning raw data into meaningful insights and crafting intelligent, data-driven solutions that solve real-world problems.<br />
+            I specialize in data analysis, visualization, and predictive modeling, along with building dynamic web applications using modern technologies like Python, React, JavaScript, and Django.<br />
+            My goal is to bridge the gap between data and development—transforming complex data into impactful, user-focused digital experiences.<br />
+            Feel free to explore my work or connect if you’d like to collaborate or discuss data, AI, or full-stack innovation.
         </div>
     )
 }
