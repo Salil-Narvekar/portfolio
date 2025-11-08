b369ffc3b6f458552f9ba51f5a2a05ecbfe16f18
Salil Narvekar <salil.enpointe@gmail.com>
2025-11-08 17:57:37 +0530

    commit

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
     <div id='About' className='grid grid-rows-8 justify-center bg-gradient-to-b from-indigo-200 to-gray-600 h-full md:h-dvh lg:h-dvh pt-20 overflow-hidden'>
 
       <div className='row-span-1 grid justify-items-center content-start' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'>
-        <span className='text-slate-800 text-md md:text-lg lg:text-3xl font-bold font-serif'> Web Developer & Freelancer </span>
-        <div className='text-slate-700 text-xs md:text-sm lg:text-md font-medium' data-aos-mirror="true" data-aos='zoom-out'> React.js . Python . Django . Fullstack </div>
+        <span className='text-slate-800 text-md md:text-lg lg:text-3xl font-bold font-serif'> Data Analyst & Freelance Web Developer </span>
+        <div className='text-slate-700 text-xs md:text-sm lg:text-md font-medium' data-aos-mirror="true" data-aos='zoom-out'>Python . React.js . SQL . Data Visualization</div>
       </div>
 
       <div className='row-span-6 grid sm:grid-cols-3 justify-items-center border-b-2 border-neutral-400 ml-16 mr-16 rounded'>
