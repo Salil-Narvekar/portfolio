1bcaf57eadbad296a046fe25d51748f22a9aa816
Salil Narvekar <salil.enpointe@gmail.com>
2025-11-13 16:04:03 +0530

    commit

--- src/Data/certificates.js
+++ src/Data/certificates.js
@@ -1,10 +1,12 @@
 import certificatesUrls from '../Urls/certificatesUrls';
 import internshipEnpointe from '../Images/internshipEnpointe.png';
 import pythonCertificate from '../Images/pythonCertificate.png';
+import mastersCertificate from '../Images/mastersCertificate.jpg';
 
 const certificates = [
     { title: 'Internship completion certificate', image: internshipEnpointe, url: certificatesUrls.internshipEnpointe },
     { title: 'Python programming certification', image: pythonCertificate, url: certificatesUrls.pythonProgramming },
+    { title: 'Masters in Data Analysis & Data Science', image: mastersCertificate, url: certificatesUrls.mastersCertificate }
 ]
 
 export default certificates
--- src/Sections/Certifications.jsx
+++ src/Sections/Certifications.jsx
@@ -1,30 +1,77 @@
-import React from 'react'
+import { useRef } from 'react'
 import ImagePlate from '../Components/CertificateComponents/ImagePlate';
 import achivementsImages from '../Data/achivementsImages'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Autoplay, Pagination } from "swiper/modules";
 import certificates from '../Data/certificates'
 
 const Certifications = () => {
+  const swiperRef = useRef(null);
 
   return (
     <div id='Certifications' className='bg-gradient-to-b from-slate-300 to-indigo-300 h-full pt-20 overflow-hidden'>
 
-      <div className='sectionTitle' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'> Certifications & Positions of Responsibity </div>
+      <div className='sectionTitle' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'>Certifications</div>
 
       <div className='bg-gradient-to-b from-indigo-200 to-gray-400 h-12'></div>
 
       <div className='grid gap-4 bg-gradient-to-b from-gray-400 to-gray-300 pr-10 md:pr-14 lg:pr-14 pl-10 md:pl-14 lg:pl-14 py-2'>
-        <div className='grid sm:grid-flow-col gap-2'>
-          {
-            certificates.map((certificate, index) => (
-              <ImagePlate key={index} index={index} src={certificate.image} title={certificate.title} onClick={() => window.open(certificate.url)} />
-            ))
-          }
-        </div>
+        <section className="certificate-swiper">
+          <Swiper
+            modules={[Navigation, Autoplay]}
+            navigation={{
+              nextEl: ".swiper-button-next-custom",
+              prevEl: ".swiper-button-prev-custom",
+            }}
+            autoplay={{
+              delay: 5000, // 5 seconds
+              disableOnInteraction: false,
+            }}
+            loop={true} // Loop the slides
+            speed={800} // Transition speed
+            slidesPerView={1}
+            onSwiper={(swiper) => (swiperRef.current = swiper)}
+            className="hero-swiper"
+          >
+            {
+              certificates.map((certificate) => (
+                <SwiperSlide key={certificate.id}>
+                  <div className="banner-slide">
+                    <div className="img-container">
+                      <img src={certificate.image} alt="Certificate image Unavailable" />
+                    </div>
+
+                    <div className="banner-overlay" onClick={() => window.open(certificate.url)}>
+                      <h1>{certificate.title}</h1>
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))
+            }
+
+            {/* Custom Navigation Arrows */}
+            <div
+              className="swiper-button-prev-custom"
+              onClick={() => swiperRef.current?.slidePrev()}
+            >
+              &#10094; {/* Left arrow */}
+            </div>
+            <div
+              className="swiper-button-next-custom"
+              onClick={() => swiperRef.current?.slideNext()}
+            >
+              &#10095; {/* Right arrow */}
+            </div>
+          </Swiper>
+        </section>
+
+        <div className='sectionTitle mb-10 mt-10' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'> Position of Responsibity </div>
 
         <div className='grid sm:grid-cols-3 sm:grid-flow-row gap-2'>
+
           {
             achivementsImages.map((achivement, index) => (
-              <ImagePlate key={index} index={index} src={achivement.image} title={achivement.title}/>
+              <ImagePlate key={index} index={index} src={achivement.image} title={achivement.title} />
             ))
           }
         </div>
