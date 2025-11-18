831bb9f7d0f7bc05df45bdddb59f45e9e0279691
Salil Narvekar <salil.enpointe@gmail.com>
2025-11-18 17:15:45 +0530

    commit

--- src/Data/certificates.js
+++ src/Data/certificates.js
@@ -1,17 +1,17 @@
 import internshipEnpointe from '../Images/internshipEnpointe.png';
 import pythonCertificate from '../Images/pythonCertificate.png';
-import mastersCertificate from '../Images/mastersCertificate.jpg';
+import mastersCertificate from '../Images/mastersCertificate.png';
 import IBM_ML_Certificate from '../Images/IBM_ML_Certificate.png';
 import IBM_Python_Certificate from '../Images/IBM_Python_Certificate.png';
 import IBM_Python101_Certificate from '../Images/IBM_Python101_Certificate.png';
 
 const certificates = [
-    { title: 'Internship completion certificate', image: internshipEnpointe },
-    { title: 'Python programming certification', image: pythonCertificate },
+    // { title: 'Internship completion certificate', image: internshipEnpointe },
     { title: 'Masters in Data Analysis & Data Science', image: mastersCertificate },
     { title: 'IBM Python for Data Analysis', image: IBM_Python_Certificate },
     { title: 'IBM Python 101 for Data Science', image: IBM_Python101_Certificate },
-    { title: 'IBM Machine Learning with Python', image: IBM_ML_Certificate }
+    { title: 'IBM Machine Learning with Python', image: IBM_ML_Certificate },
+    { title: 'Python programming certification', image: pythonCertificate }
 ]
 
 export default certificates
--- src/Sections/Certifications.jsx
+++ src/Sections/Certifications.jsx
@@ -23,45 +23,29 @@ const Certifications = () => {
               nextEl: ".swiper-button-next-custom",
               prevEl: ".swiper-button-prev-custom",
             }}
-            autoplay={{
-              delay: 5000, // 5 seconds
-              disableOnInteraction: false,
-            }}
-            loop={true} // Loop the slides
-            speed={800} // Transition speed
+            loop={true}
+            autoplay={{ delay: 5000 }}
+            speed={800}
             slidesPerView={1}
             onSwiper={(swiper) => (swiperRef.current = swiper)}
-            className="hero-swiper"
           >
-            {
-              certificates.map((certificate) => (
-                <SwiperSlide key={certificate.id}>
-                  <div className="banner-slide">
-                    <div className="img-container">
-                      <img src={certificate.image} alt="Certificate image Unavailable" />
-                    </div>
+            {certificates.map((certificate, index) => (
+              <SwiperSlide key={index}>
+                <div className="banner-slide">
+                  <div className="img-container">
+                    <img src={certificate.image} alt="" />
                   </div>
-                </SwiperSlide>
-              ))
-            }
+                </div>
+              </SwiperSlide>
+            ))}
 
-            {/* Custom Navigation Arrows */}
-            <div
-              className="swiper-button-prev-custom"
-              onClick={() => swiperRef.current?.slidePrev()}
-            >
-              &#10094; {/* Left arrow */}
-            </div>
-            <div
-              className="swiper-button-next-custom"
-              onClick={() => swiperRef.current?.slideNext()}
-            >
-              &#10095; {/* Right arrow */}
-            </div>
+            {/* ⬇  Buttons must be HERE (inside Swiper) */}
+            <div className="swiper-button-prev-custom">&#10094;</div>
+            <div className="swiper-button-next-custom">&#10095;</div>
           </Swiper>
         </section>
 
-        <div className='sectionTitle mb-10 mt-10' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'> Position of Responsibity </div>
+        <div className='sectionTitle mb-10 mt-10' data-aos-offset="100" data-aos-mirror="true" data-aos='zoom-out'>Position of Responsibility</div>
 
         <div className='grid sm:grid-cols-3 sm:grid-flow-row gap-2'>
 
