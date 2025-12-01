066483d7096334a690a2bb02c78871268af0c87b
Salil Narvekar <salil.enpointe@gmail.com>
2025-12-01 13:30:14 +0530

    commit

--- src/Urls/resumeUrl.js
+++ src/Urls/resumeUrl.js
@@ -1,2 +1,2 @@
-const resumeUrl = 'https://drive.google.com/file/d/1k2nTkCoqCqK-FcsXIPLpprfECuMznOuK/view?usp=sharing';
+const resumeUrl = 'https://drive.google.com/file/d/1jJbbT472CG1GVcmWfac-JH7Lj4UyY-oq/view?usp=sharing';
 export default resumeUrl;
