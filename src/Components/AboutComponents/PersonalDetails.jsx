0efb550adab4a8d252681b7657dabb4040da7926
Salil Narvekar <salil.enpointe@gmail.com>
2025-12-01 13:37:33 +0530

    commit

--- src/Components/AboutComponents/PersonalDetails.jsx
+++ src/Components/AboutComponents/PersonalDetails.jsx
@@ -4,14 +4,14 @@ const PersonalDetails = () => {
   return (
     <div className='grid grid-flow-row gap-2 justify-end content-start font-semibold text-slate-900 text-balance text-xs md:text-md lg:text-md xl:text-lg py-4'>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="50">- Salil Narvekar</span>
+      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="350">- 11th June, 2002</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="100">- narvekarsalil1106@gmail.com</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="150">- +91 9869757298</span>
-      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="200">- BSc.IT Graduate, Year - 2023</span>
+      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="400">- India, Maharashtra Mumbai - 400007</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="300">- Data Analyst & Freelancce Full Stack Developer</span>
+      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="200">- BSc.IT Graduate, Year - 2023</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="250">- Certified Data Analyst & Scientist with AI.</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="250">- IBM Certified Data Analyst & Scientist</span>
-      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="350">- 11th June, 2002</span>
-      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="400">- India, Maharashtra Mumbai - 400007</span>
     </div>
   )
 }
