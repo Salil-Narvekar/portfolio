23195c6f110dd3016df0011300c4566c3ec10e04
Salil Narvekar <salil.enpointe@gmail.com>
2025-12-01 13:34:27 +0530

    commit

--- src/Components/AboutComponents/PersonalDetails.jsx
+++ src/Components/AboutComponents/PersonalDetails.jsx
@@ -7,9 +7,9 @@ const PersonalDetails = () => {
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="100">- narvekarsalil1106@gmail.com</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="150">- +91 9869757298</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="200">- BSc.IT Graduate, Year - 2023</span>
+      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="300">- Data Analyst & Freelancce Full Stack Developer</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="250">- Certified Data Analyst & Scientist with AI.</span>
-      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="250">- IBM Certified Python Programmer</span>
-      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="300">- Internship - 6 months & Freelance React projects</span>
+      <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="250">- IBM Certified Data Analyst & Scientist</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="350">- 11th June, 2002</span>
       <span data-aos-offset="90" data-aos-mirror="true" data-aos='fade-right' data-aos-delay="400">- India, Maharashtra Mumbai - 400007</span>
     </div>
